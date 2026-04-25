eae03c812652ea2568eaf5270ec28d7b91b79254
LENOVO <LENOVO@DEFVINTAQY>
2026-04-25 21:06:39 +0700

    Add project files.

---
+++ LatihanMemberClass-6576/Program.cs
@@ -0,0 +1,22 @@
+﻿using LatihanMemberClass_6576;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        planeTicket ticket1 = new planeTicket();
+        ticket1.origin = "JOGJA";
+        ticket1.destination = "PKU";
+        ticket1.cost = 100;
+        ticket1.currency = "USD";
+
+        planeTicket ticket2 = new planeTicket();
+        ticket2.origin = "JKT";
+        ticket2.destination = "PKU";
+        ticket2.cost = 750000;
+        ticket2.currency = "IDR";
+
+        ticket1.PrintSummary();
+        ticket2.PrintSummary();
+    }
+}
---
+++ LatihanMemberClass-6576/planeTicket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LatihanMemberClass_6576
+{
+    internal class planeTicket
+    {
+        //propperti
+        public string origin;
+        public string destination;
+        public int cost;
+        public string currency;
+
+        //method
+        public void PrintSummary()
+        {
+            Console.WriteLine("Origin:" + origin);
+            Console.WriteLine("Destination: " + destination);
+            Console.WriteLine("Cost:" + cost + " (" + currency + ")");
+
+            if(currency == "USD")
+            {
+                int idr = cost * 15000;
+                Console.WriteLine("Convert to Rupiah: " +idr);
+            }
+            else if (currency == "IDR")
+            {
+                int usd = cost / 15000;
+                Console.WriteLine("Convert to Dollar:" +usd);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
